5cec3a7f46499859294b1e744b77513cb321ad89
darshit akbari <darshitakbari9999@gmail.com>
2023-02-17 17:26:02 +0530

    a

---
+++ project91-master/src/component/data.js
@@ -0,0 +1,30 @@
+
+const allportfolio = [
+  {
+
+    photo: "person-using.jpg",
+    contain: "Disign"
+  },
+  {
+    photo: "176_E39A2335.jpg",
+    contain: "Marketing"
+  },
+  {
+    photo: "5-book-reading.jpg",
+    contain: "Disign"
+  },
+  {
+    photo: "6697_wide_250x141_2x.jpg",
+    contain: "Devlopment"
+  },
+  {
+    photo: "This-is-.jpg",
+    contain: "Devlopment"
+  },
+  {
+    photo: "assam-india.jpg",
+    contain: "Disign"
+  }
+];
+
+export default allportfolio;
---
+++ project91-master/src/component/dataa.js
@@ -0,0 +1,15 @@
+const allslider = [
+    {
+      contain: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+      name: "Dharuvin Nakrani",
+      skill: "Web Designer",
+      photo: "0jwhEGnX_400x400.jpg"
+    },
+    {
+      contain: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+      name: "Dharuvin Nakrani",
+      skill: "Web Designer",
+      photo: "0jwhEGnX_400x400.jpg"
+    }
+  ];
+  export default allslider;
